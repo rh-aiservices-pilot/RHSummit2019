afd9b2a3eb80fd8ba9b28613e4183f01fba6ae55
Sean Pryor <spryor@redhat.com>
2019-05-29 16:04:48 -0500

    Changed xz to gz in start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-/usr/bin/tar -xvf "${DIR}/Resources.tar.xz"
+/usr/bin/tar -xvf "${DIR}/Resources.tar.gz"
 
 /usr/bin/python3 -m virtualenv "${DIR}/venv"
 source "${DIR}/venv/bin/activate"
